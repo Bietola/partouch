c0405309b854ba525df24de141ae1d320472ff14
Bietola <dincio.montesi@gmail.com>
2020-07-29 11:57:29 +0200

    Draw proto-sun

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ggez::event;
 use ggez::graphics;
 use ggez::nalgebra as na;
+use ggez::nalgebra::{Point2, Vector2};
 use ggez::{Context, GameResult};
 
 struct MainState {
@@ -13,6 +14,14 @@ impl MainState {
     }
 }
 
+fn make_sun(ctx: &mut Context, pos: Point2<f32>) -> GameResult<graphics::Mesh> {
+    let mut mb = graphics::MeshBuilder::new();
+    for i in -4..4 {
+        mb.line(&[pos, pos + Vector2::new(200., 50. * i as f32)], 4., graphics::WHITE)?;
+    }
+    mb.build(ctx)
+}
+
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         // Update player physics and handle exit
@@ -23,16 +32,8 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
-        // TODO Draw sun instead of circle
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            na::Point2::new(0.0, 0.0),
-            100.0,
-            2.0,
-            graphics::WHITE,
-        )?;
-        graphics::draw(ctx, &circle, (na::Point2::new(0., 380.0),))?;
+        let sun = make_sun(ctx, Point2::new(200., 200.))?;
+        graphics::draw(ctx, &sun, (Point2::new(0., 0.),))?;
 
         graphics::present(ctx)?;
         Ok(())
